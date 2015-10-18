42bfa311b0bc67471b1bea925f71a487913dd102
Daniel de la Fuente <ddelafuente@ziprealty.com>
2015-10-18 01:27:10 -0700

    Added about us section

--- src/AppBundle/Controller/DefaultController.php
+++ src/AppBundle/Controller/DefaultController.php
@@ -13,6 +13,38 @@ class DefaultController extends Controller
      */
     public function indexAction(Request $request)
     {
-        return $this->render('default/index.html.twig');
+      return $this->render('default/index.html.twig');
+    }
+
+    /**
+     * @Route("/about-us/mission", name="mission")
+     */
+    public function missionAction(Request $request)
+    {
+      return $this->render("default/mission.html.twig");
+    }
+
+    /**
+     * @Route("/about-us/vision", name="vision")
+     */
+    public function visionAction(Request $request)
+    {
+      return $this->render("default/vision.html.twig");
+    }
+
+    /**
+     * @Route("/products", name="products")
+     */
+    public function productsAction(Request $request)
+    {
+      return $this->render("default/products.html.twig");
+    }
+
+    /**
+     * @Route("/contact", name="contact")
+     */
+    public function contactAction(Request $request)
+    {
+      return $this->render("default/contact.html.twig");
     }
 }
