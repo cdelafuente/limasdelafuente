2c7b6ea28647276216b4519bf2623cb4f2461d3c
cdelafuente <cristhian.delafuente@gmail.com>
2016-07-07 18:15:13 -0700

    Added minification to gulp task

--- gulpfile.js
+++ gulpfile.js
@@ -1,12 +1,20 @@
 const gulp = require('gulp');
 const sass = require('gulp-sass');
+const cleanCSS = require('gulp-clean-css');
 
+/**
+ * Generate CSS files
+ */
 gulp.task('sass', function () {
   return gulp.src('web/styles/sass/**/*.scss')
     .pipe(sass().on('error', sass.logError))
+    .pipe(cleanCSS())
     .pipe(gulp.dest('web/styles/css/'));
 });
 
-gulp.task('sass:watch', function () {
-  gulp.watch('web/styles/sass/**/*.scss', [sass]);
+/**
+ * Watch SCSS files
+ */
+gulp.task('watch', function () {
+  gulp.watch('web/styles/sass/**/*.scss', ['sass']);
 });
